8cb0fa277c5551f70140239ded179f5e2167141d
rahulmcodetown <112067343+gauravjain0761@users.noreply.github.com>
2024-04-27 14:11:48 +0530

    Web Setup

---
+++ build/fancy_image_loader/generated/source/buildConfig/debug/com/example/fancy_image_loader/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.fancy_image_loader;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.example.fancy_image_loader";
+  public static final String BUILD_TYPE = "debug";
+}
